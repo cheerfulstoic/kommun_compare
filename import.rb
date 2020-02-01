2af651437c17ca9ad18f3d4fa0dc7c88bd73287b
Brian Underwood <brian@brian-underwood.codes>
2020-02-01 16:26:22 +0100

    Update code to do filtering

--- import.rb
+++ import.rb
@@ -1,55 +1,68 @@
 require 'roo-xls'
+require 'csv'
+require 'json'
 
 data = 
   Dir.glob('./data/*.xls').flat_map do |filename|
     puts filename
     _, base_name = filename.match(/\/([^\/]+)\.xls$/).to_a
-    puts "base_name: #{base_name.inspect}"
+    # puts "base_name: #{base_name.inspect}"
 
     _, subject = base_name.match(/_([^_]+)$/).to_a
-    puts "subject: #{subject.inspect}"
+    # puts "subject: #{subject.inspect}"
 
-    doc = Roo::Spreadsheet.open("./data/#{base_name}.xls", extension: :xls)
+    if %w[CO2 CO2-equivalents].include?(subject)
+      doc = Roo::Spreadsheet.open("./data/#{base_name}.xls", extension: :xls)
 
-    rows = doc.sheet(1).parse
+      rows = doc.sheet(1).parse
 
-    require 'csv'
-    require 'json'
+      # Drop first three rows
+      rows.shift
+      rows.shift
+      rows.shift
 
-    # Drop first three rows
-    rows.shift
-    rows.shift
-    rows.shift
+      year_row = rows.shift
+      header_row = rows.shift
 
-    year_row = rows.shift
-    header_row = rows.shift
-
-    header_row = year_row.zip(header_row).map do |year_row_cell, header_row_cell|
-      if year_row_cell.nil? || year_row_cell.strip == ''
-        header_row_cell
-      else
-        year_row_cell
+      header_row = year_row.zip(header_row).map do |year_row_cell, header_row_cell|
+        if year_row_cell.nil? || year_row_cell.strip == ''
+          header_row_cell
+        else
+          year_row_cell
+        end
       end
-    end
 
-    header_row.unshift('Ämne')
+      header_row.unshift('Ämne')
 
 
-    rows.map do |row|
-      row.unshift(subject)
+      rows.map do |row|
+        row.unshift(subject)
 
-      header_row.zip(row).to_h
+        d = header_row.zip(row).to_h
+        (1990..2011).each do |year|
+          d.delete(year.to_s)
+        end
+        d
+      end.reject do |d|
+        d['Huvudsektor'] == 'alla' ||
+        d['Kommun'] == 'alla' ||
+        d['Huvudsektor'] == 'Industri (energi och processer)'
+      end.select do |d|
+        d['Undersektor'] == 'alla'
+      end
+    else
+    []
     end
   end
 
-File.open('./data/output.json', 'w') { |f| f << data.to_json }
+File.open('./output/without_industry.json', 'w') { |f| f << data.to_json }
 
 keys = data.flat_map(&:keys).uniq
 
 # puts data[0].inspect
 # puts keys.inspect
 
-CSV.open('./data/output.csv', 'wb') do |csv|
+CSV.open('./output/without_industry.csv', 'wb') do |csv|
   csv << keys
 
   data.each do |record|
