0b159da23dfa7ae415fe58ee9cb3a03f81f2c752
Gr8-m8 <martinn.lindbladd@gmail.com>
2020-02-01 16:45:06 +0100

    fetch_files
    get all xls files from website and add them to the "data" folder

--- fetch_files.py
+++ fetch_files.py
@@ -1,14 +1,24 @@
 import requests
 
 prefix = "http://www.airviro.smhi.se/RUS/rapporter/"
-fin = open("files.txt","r")
+#fin = open("files.txt","r")
 outputdir = ".\data"
 
-all_lines = fin.readlines()
+dataparse = requests.get(prefix).text.split("\"")
+data = []
+
+for s in dataparse:
+	if "lansrapport_" in s:
+		if "xls" in s:
+			s = s.split("\"")
+			print(s[0])
+			data.append(s[0])
+
+all_lines = data 
 
 for file_name in all_lines:
-    fn = prefix + file_name.strip()
-    #print(fn)
+    fn = prefix + file_name
+    print(fn)
     r = requests.get(fn)
     fo = "{}\{}".format(outputdir, file_name.strip())
     open(fo, "wb").write(r.content)
