81eb74ae12274f54bca2df59a745cc36c660c751
Timo <timogos.k@live.nl>
2020-03-25 19:57:55 +0100

    Improve chart performance
    After looking at the source of vue-chartjs, I feel confident making this
    change.
    Thanks to the devtools, I discovered Vue's reactivity is what's causing
    the big performance impact. That's why I'm storing the reference to the
    chart as a normal property instead of in the Vue data.

--- chart/src/components/Trend.vue
+++ chart/src/components/Trend.vue
@@ -1,16 +1,47 @@
+<template>
+  <div>
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
 <script>
-import { Line } from 'vue-chartjs'
+import Chart from 'chart.js'
 
 export default {
   props: ['data', 'options'],
-  mixins: [Line],
   mounted () {
-    this.renderChart(this.data, this.options)
+    this.renderChart(this.data, this.options);
   },
   watch: {
     data () {
-      this.renderChart(this.data, this.options)
+      this.renderChart(this.data, this.options);
     }
-  }
+  },
+
+  // not stored in data to avoid reactivity
+  // reactivity is not needed and has a big performance impact
+  _chart: null,
+
+  methods: {
+    renderChart (data, options) {
+      if (this._chart) {
+        this._chart.destroy();
+      }
+
+      const newChart = new Chart(this.$refs.canvas, {
+        type: 'line',
+        data: data,
+        options: options
+      });
+
+      this._chart = newChart;
+    },
+  },
+  
+  beforeDestroy () {
+    if (this._chart) {
+      this._chart.destroy();
+    }
+  },
 }
 </script>
